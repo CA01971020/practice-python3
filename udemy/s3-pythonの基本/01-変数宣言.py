3b79c39b775689b3a795851c37946972fbefb9a2
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-12 13:00:12 +0900

    Initial commit

---
+++ udemy/s3-pythonの基本/01-変数宣言.py
@@ -0,0 +1,14 @@
+# 変数宣言
+# type関数で型の確認ができる
+num = 2003
+name = "hii"
+is_ok = True
+print(num,type(num),name,type(name),is_ok,type(is_ok))
+
+# 型変換
+a = "1"
+new_num = int(a)
+print(a,type(a),new_num,type(new_num))
+
+# 変数の最初を数字にするとsyntaxエラーになる。
+# 例：1num
---
+++ udemy/s3-pythonの基本/02-print出力.py
@@ -0,0 +1,9 @@
+# 空白区切り
+print("hi","mike")
+
+# 区切りの指定
+print("hi","mike",sep=",")
+
+# end はデフォルトで改行 endを空白にすると横繋がりになる
+print("hi","mike",sep=",",end="")
+print("hi","mike",sep=",",end=".")
