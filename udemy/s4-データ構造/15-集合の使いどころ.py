e348772c5f57b501f1ede254f368e338d596846f
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-15 12:17:15 +0900

    Create 15-集合の使いどころ.py

---
+++ udemy/s4-データ構造/15-集合の使いどころ.py
@@ -0,0 +1,10 @@
+my_friends = { "A" , "C" , "D"}
+A_friends = { "B" , "D" , "E" , "F"}
+
+#共通の友達を出力
+print(my_friends & A_friends)
+f = [ "ap" , "ba" ,"ap" ,"ba" ]
+
+#ユニークな要素だけ取り出し
+kind = set(f)
+print(kind)
