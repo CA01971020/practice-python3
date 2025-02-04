8688ea98092a46d3e06ea5e3a49eb43c1ed474b3
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-04 23:29:03 +0900

    Create 01-クラスの定義.py

---
+++ udemy/s7-オブジェクトとクラス/01-クラスの定義.py
@@ -0,0 +1,15 @@
+# Personクラス
+class Person(object):
+    # helloを出力する関数
+    def say_something(self):
+        print("hello")
+
+# 変数personにPersonクラスを指定
+person = Person()
+# Personクラスのsay_something関数を実行
+person.say_something()
+
+## 関数の例
+# def person(name):
+#     if name == "AA":
+#         print("hello")
