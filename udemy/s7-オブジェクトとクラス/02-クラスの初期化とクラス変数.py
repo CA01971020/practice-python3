38282de771425b5ba53511219a66178f4aa9cf43
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-04 23:37:16 +0900

    Create 02-クラスの初期化とクラス変数.py

---
+++ udemy/s7-オブジェクトとクラス/02-クラスの初期化とクラス変数.py
@@ -0,0 +1,20 @@
+# Personクラス
+class Person(object):
+    # __init__はクラスが生成されたら最初に実行される
+    def __init__(self,name):
+        # self.name に 引数のnameを格納して出力
+        self.name = name
+        print(self.name)
+
+    # selfは値を保持する役割を持つ
+    def say_something(self):
+        print("I am {}. hello".format(self.name))
+        self.run(3)
+
+    def run(self,num):
+        print("num"*num)
+
+
+
+person = Person("Mike")
+person.say_something()
