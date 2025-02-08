b8d09da360f9c971d69dd1b37e0b04d67b312e5c
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-08 23:46:12 +0900

    Update 03-クラスの継承.py

--- udemy/s7-オブジェクトとクラス/03-クラスの継承.py
+++ udemy/s7-オブジェクトとクラス/03-クラスの継承.py
@@ -1,13 +1,24 @@
 class Car(object):
+    def __init__(self,model=None):
+        self.model = model
     def run(self):
         print("run")
 
 # Carクラスを継承
 class ToyoCar(Car):
+    def run(self):
+        print("fast")
     pass
 
 # Carが持つメソッドと、独自のメソッドを使用できる
 class TesCar(Car):
+    def __init__(self, model="Model S",enable_auto_run=False):
+        # 親を呼び出す
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+    ## 親のクラスを上書きできる
+    def run(self):
+        print("super fast")
     def auto_run(self):
         print("auto run")
 
@@ -15,9 +26,11 @@ car = Car()
 car.run()
 print("#########")
 # ToyoCarの親であるCarがprint("run")を持っているので、runと出力
-toto_car = ToyoCar()
-toto_car.run()
+toyo_car = ToyoCar("Lexus")
+print(toyo_car.model)
+toyo_car.run()
 print("#########")
-tes_Car = TesCar()
+tes_Car = TesCar("Model S")
+print(tes_Car.model)
 tes_Car.run()
 tes_Car.auto_run()
